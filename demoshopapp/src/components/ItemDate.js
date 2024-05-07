2237eb25d9d4225b17afcfc1895cd861b9c35872
VivekDeyVD <vivekvd35@gmail.com>
2024-05-07 19:43:04 +0530

    react basics

---
+++ demoshopapp/src/components/ItemDate.js
@@ -0,0 +1,12 @@
+import './ItemDate.css';
+
+function ItemDate(){
+
+    return(<div className='date'>
+        <div>
+            <span>20</span>
+        </div>
+    </div>);
+}
+
+export default ItemDate;
